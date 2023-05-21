e58315b424d13da7a28f632234c966d2ee57da34
Subham Agarwal <subhamcool796@gmail.com>
2023-05-21 13:15:07 +0530

    Room List added with link

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { Nav, Navbar } from 'rsuite';
+import React, { useContext } from 'react';
+import { Loader, Nav, Navbar } from 'rsuite';
 import RoomItem from './RoomItem';
-
+import { useRooms } from '../../context/rooms.context';
+import { Link, useLocation } from 'react-router-dom';
+ 
 const ChatRoomList = ( { aboveElHeight } ) => {
+  const rooms = useRooms();
+  const location = useLocation();
+
+
+
   return (
   <Nav 
   appearance="subtle"
@@ -11,11 +18,28 @@ const ChatRoomList = ( { aboveElHeight } ) => {
   className="overflow-y-scroll custom-scroll"
   style={{
     height: `calc(100% - ${aboveElHeight}px)`,
-  }}>
-    <Nav.Item>
-        <RoomItem />
-    </Nav.Item>
-
+  }}
+  activeKey={location.pathname}
+  >
+    
+    {!rooms && (
+        <Loader center vertical 
+      content="Loading" 
+      speed='slow' 
+      size="md" />
+      )};
+      {
+      rooms && 
+      rooms.length > 0 && 
+      rooms.map(room => (
+      <Nav.Item componentClass={Link}
+       to={`/chat/${room.id}`}
+        key={room.id}
+        eventKey={`/chat/${room.id}`}
+        >
+        <RoomItem room={room} />
+      </Nav.Item>
+    ))};
   </Nav>
   );
 };
